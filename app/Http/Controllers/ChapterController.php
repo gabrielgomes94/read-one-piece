be109801958afc2ad76d3c8aaf7c87f6af1e75b0
Gabriel <gabrielgomesch@gmail.com>
2024-01-28 12:51:11 -0300

    add chapters list backend

---
+++ app/Http/Controllers/ChapterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ChapterController extends Controller
+{
+    public function get(string $chapterId): JsonResponse
+    {
+//        dd($chapterId);
+        $images = Storage::files('public/cap_' . $chapterId . '/');
+
+        $imagesPresented = collect($images)->map(function ($image) {
+            $image = preg_replace('/public/', 'storage', $image);
+
+            return asset($image);
+        });
+
+        return new JsonResponse([
+            'images' => $imagesPresented,
+        ]);
+    }
+
+    public function listChapters(): JsonResponse
+    {
+        $chapters = config('one_piece.chapters');
+
+        foreach ($chapters as $key => $value) {
+            $data[] = [
+                'value' => str_pad($key, 4, '0', STR_PAD_LEFT),
+                'label' => $value,
+            ];
+        }
+
+        return new JsonResponse([
+            'chapters' => $data ?? [],
+        ]);
+    }
+}

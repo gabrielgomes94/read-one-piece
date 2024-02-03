3336b4dbcdea1f56f6dd7712d2e663d00583b585
Gabriel <gabrielgomesch@gmail.com>
2024-02-03 12:39:44 -0300

    update config logging file

--- config/logging.php
+++ config/logging.php
@@ -55,14 +55,11 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
             'level' => env('LOG_LEVEL', 'debug'),
-            'replace_placeholders' => true,
         ],
 
         'daily' => [

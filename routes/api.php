67fc3ef213f50c1c42f010fa40c99f028604cf6b
Gabriel <gabrielgomesch@gmail.com>
2024-03-01 23:17:03 -0300

    chore: remove sanctum routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/cap/{chapter_id}', [ChapterController::class, 'get']);
 Route::get('/chapters', [ChapterController::class, 'listChapters']);

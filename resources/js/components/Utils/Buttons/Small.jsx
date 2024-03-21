b0eced23e34b879f48bac2338f1d9e73d2dbfb22
Gabriel <gabrielgomesch@gmail.com>
2024-03-21 15:35:25 -0300

    refact: extract base button for application

---
+++ resources/js/components/Utils/Buttons/Small.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Small = ({children}) => (
+    <div className="
+    hover:bg-gray-300
+    flex items-center
+    rounded p-4 m-4"
+    >
+        {children}
+    </div>
+)
---
+++ resources/js/components/Utils/Icons/ArrowRight.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ArrowRight = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="40"
+        viewBox="0 96 960 960"
+        width="40"
+    >
+        <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z"/>
+    </svg>
+)

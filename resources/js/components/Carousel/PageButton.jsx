4dc390ddfd71991ecea49b7e80255f6039dee50d
Gabriel <gabrielgomesch@gmail.com>
2024-05-27 11:51:00 -0300

    move page navigation buttons bellow image

--- resources/js/components/Carousel/PageButton.jsx
+++ resources/js/components/Carousel/PageButton.jsx
@@ -3,7 +3,11 @@ import {ArrowRight} from "../Icons/ArrowRight.jsx";
 import {ArrowLeft} from "../Icons/ArrowLeft.jsx";
 
 export const PageButton = ({onClick, type}) => (
-    <div className={"right min-h-max hover:bg-gray-300 hover:text-white flex items-center p-8 m-8"}
+    <div className={
+        "hover:bg-red-100 " +
+        "flex items-center rounded " +
+        "p-1 m-1 md:p-4 md:m-4 "
+    }
          onClick={onClick}
     >
         {
--- resources/js/components/ChapterSelector/ChapterButton.jsx
+++ resources/js/components/ChapterSelector/ChapterButton.jsx
@@ -5,7 +5,7 @@ import {DoubleArrowLeft} from "../Icons/DoubleArrowLeft.jsx";
 export const ChapterButton = ({onClick, type}) => (
     <div
         onClick={onClick}
-        className="hover:bg-gray-300 flex items-center rounded p-1 m-1 md:p-4 md:m-4"
+        className="hover:bg-red-100 flex items-center rounded p-1 m-1 md:p-4 md:m-4"
     >
         {
             type === 'next' ? <DoubleArrowRight/> : <DoubleArrowLeft/>

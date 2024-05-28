83c2f4428c7073d56344f488d2ca41acdc123d9e
Gabriel <gabrielgomesch@gmail.com>
2024-05-28 17:26:29 -0300

    refact: decouple root logic from app component

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,3 +1,34 @@
 import '../css/app.css';
-
 import './components/App.jsx'
+
+import {createRoot} from "react-dom/client";
+import {CookiesProvider} from "react-cookie";
+import {CoverStories} from "./components/CoverStories/CoverStories.jsx";
+import {Manga} from "./components/Manga/Manga.jsx";
+import {App} from "./components/App.jsx"
+import React from "react";
+
+if (document.getElementById('manga')) {
+    const container = document.getElementById('manga');
+    const root = createRoot(container);
+
+    root.render(
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
+    );
+} else if (document.getElementById('cover-stories')) {
+    const container = document.getElementById('cover-stories');
+    const root = createRoot(container);
+
+    root.render(<CoverStories listChaptersURI={'cover-stories'} />);
+} else if (document.getElementById('cover-story')) {
+    const container = document.getElementById('cover-story');
+    const root = createRoot(container);
+
+    root.render(
+        <Manga listChaptersURI={'cover-stories'}
+              chapterId={document.getElementById('cover-story').dataset.storyId}
+        />
+    );
+}
--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,31 +1,8 @@
-import React, {useEffect} from "react";
-import {createRoot} from "react-dom/client";
-import {CoverStories} from "./CoverStories/CoverStories.jsx";
+import React from "react";
 import {Manga} from "./Manga/Manga.jsx";
 import { CookiesProvider, useCookies } from 'react-cookie'
 
-if (document.getElementById('manga')) {
-    const container = document.getElementById('manga');
-    const root = createRoot(container);
-
-    root.render(
-        <CookiesProvider>
-            <App />
-        </CookiesProvider>
-    );
-} else if (document.getElementById('cover-stories')) {
-    const container = document.getElementById('cover-stories');
-    const root = createRoot(container);
-
-    root.render(<CoverStories listChaptersURI={'cover-stories'} />);
-} else if (document.getElementById('cover-story')) {
-    const container = document.getElementById('cover-story');
-    const root = createRoot(container);
-
-    root.render(<Manga listChaptersURI={'cover-stories'} chapterId={document.getElementById('cover-story').dataset.storyId} />);
-}
-
-function App() {
+export function App() {
     const [cookies, setCookie] = useCookies(['chapter', 'page'])
 
     const mangaData = document.getElementById('manga-data');
